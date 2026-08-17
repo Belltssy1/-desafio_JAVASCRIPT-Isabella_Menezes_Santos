9bb279248c1fa49f3298517fa4c9dbda43a6cd51
ISABELLA MENEZES SANTOS <026.079821@universidadesenaicimatec.edu.br>
2026-08-17 14:38:06 -0300

    Ajusta posicionamento do botao e espacamento dos checkboxes

--- js/form.js
+++ js/form.js
@@ -11,21 +11,21 @@ class contato {
 }
 
 function Post(form) {
-  
+
     if (window.event) {
         window.event.preventDefault();
     }
 
-   
+
     let data = new contato(
         form.elements.namedItem("nome").value,
-        form.elements.namedItem("sobrenome").value, 
-        form.elements.namedItem("email").value, 
-        form.elements.namedItem("cpf").value, 
-        form.elements.namedItem("telefone").value, 
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("cpf").value,
+        form.elements.namedItem("telefone").value,
         form.elements.namedItem("contato").value
     );
- console.log(data);
+    console.log(data);
 
     alert(`Obrigado sr(a) ${data.nome} ${data.sobrenome}, sua mensagem (${data.contato}) foi encaminhada com sucesso!`);
 
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const selectElement = document.querySelector('select[name="contato"]');
 
     if (selectElement) {
-      
+
         selectElement.innerHTML = `
             <option value="" disabled selected>TIPO DE CONTATO*</option>
             <option value="ELOGIO">ELOGIO</option>
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnEnviar = document.querySelector('form button[type="submit"]');
 
     if (btnEnviar) {
-       
+
         btnEnviar.style.transition = "transform 0.3s ease, background-color 0.3s ease, box-shadow 0.3s ease";
         btnEnviar.style.cursor = "pointer";
 
@@ -69,11 +69,31 @@ document.addEventListener("DOMContentLoaded", function () {
             btnEnviar.style.boxShadow = "0px 4px 12px rgba(0, 0, 0, 0.3)";
         });
 
-   
+
         btnEnviar.addEventListener("mouseleave", function () {
             btnEnviar.style.transform = "scale(1)";
-            btnEnviar.style.backgroundColor = ""; 
+            btnEnviar.style.backgroundColor = "";
             btnEnviar.style.boxShadow = "none";
         });
     }
+    // Função para habilitar/desabilitar o botão com base no primeiro checkbox
+
 });
+
+
+function toggleSubmitButton() {
+    const termsCheck = document.getElementById("termsCheck");
+    const btnEnviar = document.getElementById("btnEnviar");
+
+    if (termsCheck && btnEnviar) {
+        if (termsCheck.checked) {
+            btnEnviar.disabled = false;
+            btnEnviar.style.opacity = "1";
+            btnEnviar.style.cursor = "pointer";
+        } else {
+            btnEnviar.disabled = true;
+            btnEnviar.style.opacity = "0.5";
+            btnEnviar.style.cursor = "not-allowed";
+        }
+    }
+}
